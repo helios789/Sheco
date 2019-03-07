542afe482bdef1a99d50e8f8da3d17a81aec0bfd
helios789 <helios789@naver.com>
2019-03-07 12:08:15 +0900

    initial Setting

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/helios789/catkin_ws/src
+# Build directory: /home/helios789/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_first_ros_pkg)

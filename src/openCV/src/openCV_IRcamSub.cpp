fd8bbe4524b93c6ef59a5a6905f6eb43290d1811
helios789 <helios789@naver.com>
2019-03-26 20:13:15 +0900

    subscribe 4개로 분리

--- src/openCV/src/openCV_IRcamSub.cpp
+++ src/openCV/src/openCV_IRcamSub.cpp
@@ -9,14 +9,97 @@
 
 using namespace cv;
 
-void msgCallback(const std_msgs::Float32MultiArray::ConstPtr& msg)
+
+const int MSG_ARRAY_ROW = 6;
+const int MSG_ARRAY_COL = 32;
+float temperatureArray[768];
+
+Mat HeatMap(Size(24, 32), CV_8UC1);
+Mat colorHeatMap;
+
+void msgCallback1(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
-    ROS_INFO("---------");
+    ROS_INFO("1---------");
 
-    for(int i = 0; i < 32; i++)
-        ROS_INFO("%4f", msg->data[i]);
+    for(int row = 0; row < MSG_ARRAY_ROW; row++)
+    {
+        for(int col = 0; col < MSG_ARRAY_COL; col++)
+        {
+           // ROS_INFO("%f",msg->data[row * MSG_ARRAY_COL + col]);
+           HeatMap.at<float>(row + MSG_ARRAY_ROW * 0, col) = msg->data[row * MSG_ARRAY_COL + col];
+           ROS_INFO("%f",HeatMap.at<float>(row + MSG_ARRAY_ROW * 0, col));
+        }
+    }
+    applyColorMap(HeatMap, colorHeatMap, COLORMAP_HOT);
+    imshow( "HeatMap", colorHeatMap );
+    waitKey(10);
 
-    ROS_INFO("---------");
+    ROS_INFO("1---------");
+}
+
+void msgCallback2(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    ROS_INFO("2---------");
+
+    for(int row = 0; row < MSG_ARRAY_ROW; row++)
+    {
+        for(int col = 0; col < MSG_ARRAY_COL; col++)
+        {
+           // ROS_INFO("%f",msg->data[row * MSG_ARRAY_COL + col]);
+           HeatMap.at<float>(row + MSG_ARRAY_ROW * 1, col) = msg->data[row * MSG_ARRAY_COL + col]; 
+            ROS_INFO("%f",HeatMap.at<float>(row + MSG_ARRAY_ROW * 1, col));    
+        }
+    }
+
+    applyColorMap(HeatMap, colorHeatMap, COLORMAP_HOT);
+    imshow( "HeatMap", colorHeatMap );
+    waitKey(10);
+
+    ROS_INFO("2---------");
+    
+}
+
+void msgCallback3(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    ROS_INFO("3---------");
+
+    for(int row = 0; row < MSG_ARRAY_ROW; row++)
+    {
+        for(int col = 0; col < MSG_ARRAY_COL; col++)
+        {
+          //  ROS_INFO("%f",msg->data[row * MSG_ARRAY_COL + col]);
+           HeatMap.at<float>(row + MSG_ARRAY_ROW * 2, col) = msg->data[row * MSG_ARRAY_COL + col];  
+         ROS_INFO("%f",HeatMap.at<float>(row + MSG_ARRAY_ROW * 2, col));  
+        }
+    }
+
+    applyColorMap(HeatMap, colorHeatMap, COLORMAP_HOT);
+    imshow( "HeatMap", colorHeatMap );
+    waitKey(10);
+
+    ROS_INFO("3---------");
+}
+
+void msgCallback4(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    ROS_INFO("4---------");
+
+    for(int row = 0; row < MSG_ARRAY_ROW; row++)
+    {
+        for(int col = 0; col < MSG_ARRAY_COL; col++)
+        {
+          //  ROS_INFO("%f",msg->data[row * MSG_ARRAY_COL + col]);
+           HeatMap.at<float>(row + MSG_ARRAY_ROW * 3, col) = msg->data[row * MSG_ARRAY_COL + col]; 
+            ROS_INFO("%f",HeatMap.at<float>(row + MSG_ARRAY_ROW * 3, col));    
+        }
+    }
+
+    applyColorMap(HeatMap, colorHeatMap, COLORMAP_HOT);
+    imshow( "HeatMap", colorHeatMap );
+    waitKey(10);
+
+    ROS_INFO("4---------");
+
 }
 
 int main(int argc, char **argv)
@@ -31,7 +114,12 @@ int main(int argc, char **argv)
     // 서브스크라이버 ros_tutorial_sub 를 작성한다. 토픽명은 "ros_tutorial_msg" 이며,
     // 서브스크라이버 큐(queue) 사이즈를 100개로 설정한다는 것이다
 
-    ros::Subscriber sub_IRcamera = nh.subscribe("IRcam", 100, msgCallback);
+    ros::Subscriber sub_IRcamera1 = nh.subscribe("IRcam1", 100, msgCallback1);
+    ros::Subscriber sub_IRcamera2 = nh.subscribe("IRcam2", 100, msgCallback2);
+    ros::Subscriber sub_IRcamera3 = nh.subscribe("IRcam3", 100, msgCallback3);
+    ros::Subscriber sub_IRcamera4 = nh.subscribe("IRcam4", 100, msgCallback4);
+
+
     // 콜백함수 호출을 위한 함수로써, 메시지가 수신되기를 대기,
     // 수신되었을 경우 콜백함수를 실행한다
     ros::spin();
